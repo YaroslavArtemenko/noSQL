28708bb51dfe77ade2c1bc98a267aa4842f18a43
YaroslavArtemenko <Yaroslavhik@yandex.ua>
2019-12-26 08:57:43 +0200

    'courseUPLOAD'

---
+++ Course_Work/Forms/EventFormEdit.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, DateField, HiddenField
+from wtforms import validators
+
+
+class EventFormEdit(FlaskForm):
+    event_name = HiddenField("Name: ", [validators.Length(3, 20, "Name should be from 3 to 20 symbols")])
+    event_date = DateField("Date: ", [validators.DataRequired("Please enter date of Event.")])
+
+
+    submit = SubmitField("Save")
+
+    def validate_date(self):
+        return bool(self.event_date.data.year > 2018)
